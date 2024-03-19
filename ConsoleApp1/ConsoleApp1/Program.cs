b0bc05f4d2085116d48504c12b8c40dfcb2c2e0a
Mikołaj Kowalski <s27630@pjwstk.edu.pl>
2024-03-19 13:31:23 +0100

    zmiana2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static double GetAvg(int[] arr)
+    {
+        int sum = 0;
+        foreach (int num in arr)
+        {
+            sum += num;
+        }
+        return (double)sum / arr.Length;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        double average = GetAvg(numbers);
+        Console.WriteLine("Average: " + average);
+    }
+}
